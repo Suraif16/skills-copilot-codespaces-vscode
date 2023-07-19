c25b1dacb1bf41b06fa9c496ea69ebdd22e4727f
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-07-19 19:18:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var skills = document.getElementById("skills");
+  var skillsMember = document.getElementById("skillsMember");
+  var skillsMemberValue = skillsMember.value;
+
+  if (skillsMemberValue == "1") {
+    skills.style.display = "block";
+  } else {
+    skills.style.display = "none";
+  }
+}
