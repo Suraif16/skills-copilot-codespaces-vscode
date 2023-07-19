ce97fff0d66a6487eb67aaeeeffba6ff20e68ddd
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-07-19 20:00:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,86 +1,89 @@
-// create web server
-const express = require('express');
-// create router
-const router = express.Router();
-// import comments model
-const commentsModel = require('../models/commentsModel');
-// import users model
-const usersModel = require('../models/usersModel');
-// import article model
-const articleModel = require('../models/articleModel');
-// import moment
-const moment = require('moment');
-// import data
-const data = require('../data.json');
-// import multer
-const multer = require('multer');
-// import path
-const path = require('path');
-// import fs
-const fs = require('fs');
+// Create web server
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-// 创建存储对象
-const storage = multer.diskStorage({
-    // 目标文件夹
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../public/images'))
-    },
-    // 目标文件名
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
-})
-// 创建上传对象
-const upload = multer({
-    storage: storage
-})
+// Use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// get comments list
-router.get('/list', (req, res) => {
-    // get params
-    const params = req.query;
-    // get page
-    const page = parseInt(params.page || 1);
-    // get size
-    const size = parseInt(params.size || 5);
-    // get total
-    const total = data.comments.length;
-    // get total page
-    const pages = Math.ceil(total / size);
-    // get start
-    const start = (page - 1) * size;
-    // get end
-    const end = page * size;
-    // get comments
-    const comments = data.comments.slice(start, end);
-    // return data
-    res.json({
-        code: 200,
-        msg: 'success',
-        data: {
-            comments,
-            page,
-            size,
-            total,
-            pages
-        }
-    })
-})
+// Use cors
+app.use(cors());
 
-// get comment by id
-router.get('/detail', (req, res) => {
-    // get params
-    const params = req.query;
-    // get id
-    const id = parseInt(params.id);
-    // get comment
-    const comment = data.comments.find(comment => comment.id === id);
-    // return data
-    res.json({
-        code: 200,
-        msg: 'success',
-        data: comment
-    })
-})
+// Connect DB
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/itcamp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// add comment
+// Create schema
+const CommentSchema = mongoose.Schema({
+  name: String,
+  message: String,
+});
+
+// Create model
+const CommentModel = mongoose.model("comments", CommentSchema);
+
+// Create route
+app.get("/comments", (req, res) => {
+  CommentModel.find({}, (err, data) => {
+    res.json(data);
+  });
+});
+
+// Create route
+app.post("/comments", (req, res) => {
+  const name = req.body.name;
+  const message = req.body.message;
+
+  const comment = new CommentModel({
+    name: name,
+    message: message,
+  });
+
+  comment.save((err, data) => {
+    res.json(data);
+  });
+});
+
+// Create route
+app.delete("/comments/:id", (req, res) => {
+  const id = req.params.id;
+
+  CommentModel.findByIdAndDelete(id, (err, data) => {
+    res.json(data);
+  });
+});
+
+// Create route
+app.get("/comments/:id", (req, res) => {
+  const id = req.params.id;
+
+  CommentModel.findById(id, (err, data) => {
+    res.json(data);
+  });
+});
+
+// Create route
+app.put("/comments/:id", (req, res) => {
+  const id = req.params.id;
+  const name = req.body.name;
+  const message = req.body.message;
+
+  CommentModel.findById(id, (err, data) => {
+    data.name = name;
+    data.message = message;
+    data.save((err, data) => {
+      res.json(data);
+    });
+  });
+});
+
+// Listen
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
